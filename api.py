12a45cef17cce134b3a93ecff7874fdbf1fc8974
jogisuda <jogisuda@hotmail.com>
2020-10-19 16:39:39 -0300

    Add files via upload

--- api.py
+++ api.py
@@ -14,4 +14,4 @@ def json_example():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=5000) #run app in debug mode on port 5000
+    app.run(host="0.0.0.0", port=port) #run app in debug mode on port 5000
