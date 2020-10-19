0a305fe583dfff947a1fa9fd3a0118392df24f91
jogisuda <jogisuda@hotmail.com>
2020-10-19 16:29:46 -0300

    Add files via upload

--- api.py
+++ api.py
@@ -2,10 +2,10 @@ from flask import Flask, request #import main Flask class and request object
 
 app = Flask(__name__) #create the Flask app
 
-@app.route('/json-example', methods=['POST']) #GET requests will be blocked
+@app.route('/', methods=['POST']) #GET requests will be blocked
 def json_example():
     return 'data received: \n{}'.format(request.get_json())
 
 
-if _name == '__main_':
+if __name__ == '__main__':
     app.run(debug=True, port=5000) #run app in debug mode on port 5000
